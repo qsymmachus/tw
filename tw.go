27121a7ed64710c9a9a09bfd798954c6f79a1eab
John Olmsted <john@olmsted.io>
2022-07-28 10:43:51 -0400

    Fix flag parsing

--- tw.go
+++ tw.go
@@ -23,19 +23,24 @@ type WeatherData struct {
 }
 
 func main() {
-	location := flag.String("location", "05401", "Where would you like to track temperature? (e.g. 05401, 'Moscow', 'SFO')")
+	location := flag.String("loc", "05401", "The location where would you like to track temperature (e.g. 05401, Moscow, SFO)")
+	verbose := flag.Bool("v", false, "Verbose mode enables additional logging")
+	flag.Parse()
 
 	weatherURL := fmt.Sprintf(BaseWeatherURL, *location)
-	weatherData, err := loadWeatherData(weatherURL)
+	weatherData, err := loadWeatherData(weatherURL, *verbose)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	fmt.Printf("%v\n", weatherData)
 }
 
-func loadWeatherData(url string) (WeatherData, error) {
+func loadWeatherData(url string, verbose bool) (WeatherData, error) {
 	var weatherData WeatherData
+
+	if verbose {
+		fmt.Printf("Fetching weather data from %s\n", url)
+	}
 
 	response, err := http.Get(url)
 	if err != nil {
