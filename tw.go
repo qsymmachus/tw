4ad3dcbf1ec6cdca9cc9824c87af64b664f5a48c
John Olmsted <john@olmsted.io>
2022-07-29 09:27:27 -0400

    Main function fetches weather and checks if it surpassed temp threshold

--- tw.go
+++ tw.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 const (
@@ -25,14 +26,44 @@ type WeatherData struct {
 func main() {
 	location := flag.String("loc", "05401", "The location where would you like to track temperature (e.g. 05401, Moscow, SFO)")
 	verbose := flag.Bool("v", false, "Verbose mode enables additional logging")
+	useCelsius := flag.Bool("c", false, "Use celsius temperature measurements, otherwise defaults to Fahrenheit")
+	tempThreshold := flag.Int("temp", 75, "The temperature threshold to watch for, in fahrenheit. Use the '-c' flag to use celsius instead.")
 	flag.Parse()
 
-	weatherURL := fmt.Sprintf(BaseWeatherURL, *location)
-	weatherData, err := loadWeatherData(weatherURL, *verbose)
+	passedThreshold, currentTemp, err := checkTempThreshold(*location, *tempThreshold, *useCelsius, *verbose)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Something went wrong trying to fetch the weather: %v", err)
 	}
-	fmt.Printf("%v\n", weatherData)
+
+	if passedThreshold {
+		fmt.Printf("It's %d˚ out! That's hotter than your %d˚ threshold.\n", currentTemp, *tempThreshold)
+	} else {
+		fmt.Printf("It's %d˚ degrees out, which is below your %d˚ degree threshold.\n", currentTemp, *tempThreshold)
+	}
+}
+
+// Checks if the current temperature at a specific location has surpassed a
+// specified temperature threshold. If it has, returns `true` as the first argument,
+// otherwise it returns `false`. The current temperature is returned as the second
+// argument. An error is returned if it fails to fetch weather data.
+func checkTempThreshold(location string, tempThreshold int, useCelsius, verbose bool) (passedThreshold bool, currentTemp int, err error) {
+	weatherURL := fmt.Sprintf(BaseWeatherURL, location)
+	weatherData, err := loadWeatherData(weatherURL, verbose)
+	if err != nil {
+		return false, currentTemp, err
+	}
+
+	if useCelsius {
+		currentTemp, err = strconv.Atoi(weatherData.CurrentCondition[0].TempCelsius)
+	} else {
+		currentTemp, err = strconv.Atoi(weatherData.CurrentCondition[0].TempFahrenheit)
+	}
+
+	if err != nil {
+		return false, currentTemp, nil
+	}
+
+	return currentTemp > tempThreshold, currentTemp, nil
 }
 
 func loadWeatherData(url string, verbose bool) (WeatherData, error) {
